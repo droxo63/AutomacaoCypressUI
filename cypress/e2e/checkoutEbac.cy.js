6c3a9d8a5ea85c239e9693d6cc0c03aa69e00286
Diego <droxo63@gmail.com>
2025-02-25 12:37:49 -0300

    update

--- cypress/e2e/checkoutEbac.cy.js
+++ cypress/e2e/checkoutEbac.cy.js
@@ -3,18 +3,20 @@ import { firstName, lastName, phone, email, senha } from "../fixtures/dados.json
 
 describe('Checkout dos produtos no carrinho', () => {
 before(() => {
-    cy.setCookie('ebacStoreVersion', 'v2', { domain: 'lojaebac.ebaconline.art.br' });
+   // cy.setCookie('ebacStoreVersion', 'v2', { domain: 'lojaebac.ebaconline.art.br' });
   });
 
   beforeEach(() => {
-    cy.visit("/home");
-    cy.get("[href='/Tab/Account']").click();
+    cy.visit("/produtos");
+    
   });
 
   it('Passo a passo de checkout de produtos', () => {
-    cy.cadastro();
-    cy.visit("/home").click()
-    cy.get(cy.get(':nth-child(2) > .r-18u37iz > :nth-child(1) > [data-testid="productDetails"]')).click()
-   
+    cy.get('.post-3073 > .product-block > .block-inner > .image > .product-image > .image-hover').click()
+    cy.get('.button-variable-item-32').scrollIntoView().click({ force: true });
+    cy.get('.button-variable-item-Brown').scrollIntoView().click();
+    cy.get('.plus').click()
+    cy.get('.single_add_to_cart_button',{ timeout: 10000 }).should('be.visible').click({ force: true });
+    
   });
 })
